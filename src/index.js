9f4c5f96193475e37f779dbc388ab22994509609
NeilSolomon <neilsolomon89@gmail.com>
2021-01-20 20:19:24 -0500

    added service worker alerts

--- src/index.js
+++ src/index.js
@@ -14,4 +14,7 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.register({
+  onSuccess: () => console.log("onSuccess"),
+  onUpdate: (reg) => console.log("onUpdate", reg),
+});
