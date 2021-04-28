18a6beec78341bc44c10ffff0b529ce1259a2c6a
NeilSolomon <neilsolomon89@gmail.com>
2021-04-28 17:28:35 -0400

    Made Arduino pwm pins use max frequency. Updated Schematic

--- ArduinoCode/frog.ino
+++ ArduinoCode/frog.ino
@@ -27,7 +27,7 @@ const double beta = (chassisWidth * tan(wheelRollerAngle) + chassisLength) / tan
 // a motor driver uses two pins to communicate: 1 for motor velocity (pwm), 1 for motor direction (high/low)
 CytronMD motor1(PWM_DIR, 2, 22); // front-left
 CytronMD motor2(PWM_DIR, 3, 23); // front-right
-CytronMD motor3(PWM_DIR, 4, 24); // rear-left
+CytronMD motor3(PWM_DIR, 6, 24); // rear-left
 CytronMD motor4(PWM_DIR, 5, 25); // rear-right
 CytronMD motors [] = {motor1, motor2, motor3, motor4};
 
@@ -55,14 +55,14 @@ void setup() {
   Serial.begin(serialRate);
   delay(100);
   
-  Serial.println("setup...");
+  Serial.println("SETUP...");
   
-  bluetooth_setup();
   uv_setup();
   door_setup();
-//  motors_setup();
+  motors_setup();
+  bluetooth_setup();
 //  battery_setup();
-  Serial.println("setup done");
+  Serial.println("SETUP DONE");
   
   Serial.println();
 }
@@ -151,11 +151,31 @@ void motors_setup() {
   pinMode(23, OUTPUT);
   pinMode(24, OUTPUT);
   pinMode(25, OUTPUT);
+
+  /* Set pwm pins to maximum frequency https://forum.arduino.cc/t/mega-2560-pwm-frequency/71434/19 
+   The 3 LSB of registers TCCRnB contain the prescale value of the frequency. The frequeny used on the pins
+   is some frequency divided by the prescale factor, so the lower the scaling factor the higher the frequency.
+   The default value of prescale for TCCR3B and TCCR4B 4->490Hz. TCCR3B controls pins 5, 3, and 2, and TCCR4B controls pins 6, 7, and 8.
+   We can't use pins 4 or 13 for pwm because their frequency is controlled by TCCR0B which also controls the clock frequency for the whole Arduino!
+   We set TCCR3B and TCCR4B to 1 -> 31 kHz*/
+  int prescaleClear = 0x07;
+  int prescaleValue = 0x01;
+  TCCR3B &= ~prescaleClear;
+  TCCR3B |= prescaleValue;
+  TCCR4B &= ~prescaleClear;
+  TCCR4B |= prescaleValue;
+  Serial.print("    TCCR3B ");
+  Serial.println(TCCR3B);
+  Serial.print("    TCCR4B ");
+  Serial.println(TCCR4B);
   
   for (int i = 0; i < 4; i++) {
     motors[i].setSpeed(0);  
   }
 
+  return;
+  
+  // test motors
   for (int i = 0; i < 4; i++) { // each motor
     Serial.print("    motor ");
     Serial.print(i+1);
@@ -340,9 +360,9 @@ double calculateMotorVelocity(int motorNumber, int xVel, int yVel, int rotVel) {
 
   // compress the value
   if (motorVelocity > 0) {
-    motorVelocity = motorVelocity * (1 - .1*motorSpeedCompression) + 20*motorSpeedCompression;
+    motorVelocity = motorVelocity * (1 - .1 * motorSpeedCompression) + 25 * motorSpeedCompression;
   } else if (motorVelocity < 0) {
-    motorVelocity = motorVelocity * (1 - .1*motorSpeedCompression) - 20*motorSpeedCompression;
+    motorVelocity = motorVelocity * (1 - .1 * motorSpeedCompression) - 25 * motorSpeedCompression;
   }
   
   return (int)floor(motorVelocity);
